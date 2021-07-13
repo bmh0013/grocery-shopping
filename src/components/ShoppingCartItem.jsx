3cd4d78394287cdf7555e0263c4f0555f8689826
Brandon Harden <bmh0013@gmail.com>
2021-07-13 16:49:15 -0500

    final styling changes

--- src/components/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ShoppingCartItem = ({ item }) => {
   return (
-    <div className="table-row">
-      <div className="table-item name">{item.name}</div>
-      <div className="table-item price">${item.price.toFixed(2)}</div>
-      <div className="table-item quantity">{item.quantity}</div>
-      <div className="table-item total">${ (item.quantity * item.price).toFixed(2) }</div>
+    <div className="cart-row">
+      <div className="cart-item name">{item.name}</div>
+      <div className="cart-item price">${item.price.toFixed(2)}</div>
+      <div className="cart-item quantity">{item.quantity}</div>
+      <div className="cart-item total">${ (item.quantity * item.price).toFixed(2) }</div>
     </div>
   );
 };
